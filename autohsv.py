e611da01d07b10d6691b8f8caf4b362261e0136b
joshjaysalazar <joshjaysalazar@gmail.com>
2019-04-16 13:10:29 -0500

    Initial commit of script

---
+++ autohsv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# AutoHSV
+# by Joshua Jay Salazar
+# Released under the GNU General Public License
+
+import sys
+import PIL
+import colors
+
+# Parse the arguments given
+PREFIX = sys.argv[1]
+FILE = sys.argv[2]
+DEST = sys.argv[3]
+
+# Create list of colors from colors.txt
+print (colors.COLORS[2][0])
