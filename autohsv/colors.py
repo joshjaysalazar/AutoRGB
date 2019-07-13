d95f12b0dfc02dbd16f6d426839275f80573f912
joshjaysalazar <joshjaysalazar@gmail.com>
2019-07-13 13:59:47 -0500

    No longer need this file

--- autohsv/colors.py
+++
@@ -1,8 +0,0 @@
-COLORS = [
-    # Hue ranges from 0 to 360, Sat and Val range from 0 to 100
-    ["White", 0, 0, 100],
-    ["Red", 0, 100, 100],
-    ["Orange", 25, 100, 100],
-    ["Yellow", 63, 75, 100],
-    ["Cyan", 176, 100, 100]
-]
